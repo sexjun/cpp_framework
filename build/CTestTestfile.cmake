f01d421dcbc184ba019a0927d41fc0427a22601c
chendongsheng <sexluna@outlook.com>
2022-02-07 22:34:51 +0800

    del build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chendongsheng/github/cpp_framework
-# Build directory: /Users/chendongsheng/github/cpp_framework/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/spdlog")
--- build/lib/spdlog/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chendongsheng/github/cpp_framework/lib/spdlog
-# Build directory: /Users/chendongsheng/github/cpp_framework/build/lib/spdlog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
