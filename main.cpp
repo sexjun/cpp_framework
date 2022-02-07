2137bfcd6b0c53dc99c10db4fbcb40fe4769dbdf
chendongsheng <sexluna@outlook.com>
2022-02-07 22:07:13 +0800

    add loginit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "toolHead.h"
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
-    toolHeadTest();
+static void logInit() {
     spdlog::cfg::load_env_levels();
     spdlog::set_level(static_cast<level::level_enum>(SPDLOG_LEVEL_INFO));
     spdlog::error("malloc failed");
+    return;
+}
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+    toolHeadTest();
+    logInit();
 }
